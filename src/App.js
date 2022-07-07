be321c2cc35dd429ffbfa10d2f4efd4ab1ca4ca1
Mishu <mishutrz2@yahoo.com>
2022-07-07 04:12:54 +0300

    works fine

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
 
   const handleFilters = (ingredients) => {
     setIngredients(ingredients);
+    renderDrinks();
   };
 
   const handleDrinkType = (type) => {
@@ -22,7 +23,17 @@ function App() {
         {drinks.map((val, idx) => {
           let available = true;
           let show = true;
+
+          // hide if another type is selected
           if (val.type !== type && type !== "all") show = false;
+
+          // hide depending on selected filters
+          ingredients.forEach((el) => {
+            if (val.ingredients.indexOf(el) === -1 && ingredients.length > 0)
+              show = false;
+          });
+
+          // gray out unavailable drinks
           val.ingredients.forEach((el) => {
             if (availableIngredients.indexOf(el) === -1) available = false;
           });
